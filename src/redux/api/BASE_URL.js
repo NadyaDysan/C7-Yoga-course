0da478c20a9e5a9a85223999d7fd1cd5567e48ce
NadyaDysan <npryanishnikova18@gmail.com>
2023-10-30 13:16:21 +0100

    Change of backend

--- src/redux/api/BASE_URL.js
+++ src/redux/api/BASE_URL.js
@@ -1,5 +1,5 @@
 import { fetchBaseQuery } from '@reduxjs/toolkit/query'
 
-export const BASE_URL = 'https://painassasin.online/';
+export const BASE_URL = 'https://skypro-music-api.skyeng.tech/';
 
 export const publicQuery = fetchBaseQuery({ baseUrl: BASE_URL });
